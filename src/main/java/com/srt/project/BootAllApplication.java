70ec7e0c190c284077b17795e4ffa310732cb793
Rajini Mohanram <rajini.mohanram@cognizant.com>
2022-05-24 22:52:19 +0530

    Basic Setup

---
+++ src/main/java/com/srt/project/BootAllApplication.java
@@ -0,0 +1,13 @@
+package com.srt.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srt/project/BootAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
